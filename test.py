38ff24b6b80e08998fa3e637db80cbe9cd79c5ae
XavierAlpha <xavierzhangalpha@outlook.com>
2023-02-12 17:49:12 +0800

    fix and remove __pycache__

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from tosgf import ToSgf, Detect_Chess, Preprocess, Recongnition_Num
 import os
 import multiprocessing
-import time
-import asyncio
-import concurrent
+from log import logger
+import glob
+
+""" Test to complete chess's information"""
 
 testpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test')
 
@@ -26,12 +27,11 @@ def saveto_chess_seq(chess, k):
 
 def saveto_chess_num(recogn, i):
         num_path = os.path.join(testpath, "numbers{0:d}.txt".format(i))
+        number = [str(chess.number) for chess in recogn.chess]
+        number.sort(key=lambda i: int(i))
+        number = ' '.join(number)
         with open(num_path, 'a', encoding='utf-8') as f:
-            for i in range(len(recogn.chess)):
-                if (i == 100):
-                    f.writelines(list(os.linesep))
-                f.writelines(list(str(recogn.chess[i].number) + ' '))
-
+            f.writelines(number)
 
 def test_prog(path, i):
         pre_img = Preprocess(path)
@@ -42,6 +42,7 @@ def test_prog(path, i):
         ToSgf.show_img(board, 5, "Board %d" % i)
     
         chess = Detect_Chess(board)
+        chess.chess_diameter()
         chess.go_matrix() # draw
     
         chess.cut()
@@ -52,24 +53,34 @@ def test_prog(path, i):
         saveto_chess_seq(chess, i)
     
         recogn = Recongnition_Num(chess.chess)
-        recogn.gen_dataset()
         recogn.load_model()
         recogn.gen_chess_num()
         saveto_chess_num(recogn, i)
 
-def main():
-    thread = []
-    path = [os.path.join(testpath, 'test1.png'), 
-            os.path.join(testpath, 'test2.png'), 
-            os.path.join(testpath, 'test3.png')]
-    for i in range(len(path)):
-        thd = multiprocessing.Process(target=test_prog, args=(path[i], i))
-        thread.append(thd)
-        time.sleep(i)
-        thd.start()
-    
-    for thd in thread:
-        thd.join()
+def test_chess_information():
+    """ 
+    Source files in test: test1.png, test2.png and test3.png;
+    The chess images were saved to test/chess{i}, and after cutting to pieces of number, it was saved to seq{i};
+    The result of each test{i}.png was saved to numbers{i}.txt, which shows sequence number of each chess.
+    """
+    process = []
+    path = glob.glob(os.path.join(testpath, "*.png"))
+    for i in range(0, len(path)//2 * 2, 2):
+        task_group = path[i:i+2]
+        for j in range(2):
+            pro = multiprocessing.Process(target=test_prog, args=(task_group[j], i+j))
+            logger.info("Process {0:d} executes!".format(i+j))
+            pro.start()
+            process.append(pro)
+        
+        process[-2].join()
+        process[-1].join()
+
+    if len(path) % 2 == 1:
+        pro = multiprocessing.Process(target=test_prog, args=(path[-1], len(path)-1))
+        pro.start()
+        pro.join()
 
 if __name__ == "__main__":
-    main()
+    test_chess_information()
+    
--- tosgf.py
+++ tosgf.py
@@ -20,8 +20,9 @@ class _Path_pool:
 
 
 class ToSgf(_Path_pool):
-    def __init__(self, img_paths: list) -> None:
+    def __init__(self, img_paths: list, train_template_paths: list) -> None:
         super().__init__(img_paths)
+        self._template_path = train_template_paths
 
     @staticmethod
     def show_img(image, sec=5, name="image"):
@@ -30,12 +31,12 @@ class ToSgf(_Path_pool):
     @staticmethod
     def saveto(filename:str, image, path: str):
         chess.saveto(filename, image, path)
-
+    
     def run(self):
         logger.info("Start run()")
 
-        template.train_template()
-        
+        template.train_template(self._template_path)
+
         for img_path in self.take():
             logger.info("Process image from {0}".format(img_path))
             pre_img = Preprocess(img_path)
@@ -43,32 +44,20 @@ class ToSgf(_Path_pool):
             vertex, area = pre_img.find_board(pre_board)
             board = pre_img.board(vertex)
             pre_img = Detect_Chess(board)
+            pre_img.chess_diameter()
             pre_img.cut()
             logger.info("Cut {0:d} chess circles of board.".format(len(pre_img.chess)))
             pre_img.gen_color()
             pre_img.detect_number()
         
             recogn = Recongnition_Num(pre_img.chess)
-            if not os.path.exists(recogn.dataset_path):        
-                recogn.preprocess_template_images() # move "template/0 , 1, 2 ..., 9" 's *png to "tmp/dataset"
-            else:
-                logger.warning("Datasets exists! Ignore preprocess_template_images().")
-            
-            recogn.gen_dataset()
-            
-            x_train, x_test, y_train, y_test = recogn.gen_train_test()
-        
-            if not os.path.exists(recogn.model_path):
-                recogn.train_network(x_train, x_test, y_train, y_test)
-            else:
-                logger.warning("Network exists! Ignore train_network().")
-            
-            logger.info("Loading model from {0}".format(recogn.model_path))
             recogn.load_model()
-
             recogn.gen_chess_num()
             logger.info("End processing image from {0}".format(img_path))
 
             # @TODO: now all chess information was complete. generate SGF format
         logger.info("End run()")
-        
+
+if __name__ == "__main__":
+   ins = ToSgf(["./opencvs/src/src1.png"], ["./template"])
+   ins.run()
